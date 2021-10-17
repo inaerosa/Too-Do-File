140e817816a340c648c7bdcd38fde229c666c9ff
Inaê Rosa <inaedb.rosa@gmail.com>
2021-10-17 18:28:03 -0300

    fix testUser

--- TestUser.java
+++ TestUser.java
@@ -1,21 +1,18 @@
-import java.io.IOException;
-import java.util.List;
-
 public class TestUser {
-    public static void main(String[] args) throws IOException {
-       User inae = new User("Inae", "inae@inae", 1);
-       SaveUser sv = new SaveUser();
 
-        User usuario = sv.get(1);
-        usuario.setName("Inaê");
-        sv.update(usuario);
+    public static void main(String[] args) {
+        User inae = new User("Inae", "inae@inae", 1);
+        User nina = new User("Nina", "nina@au", 2);
 
+        SaveUser sv = new SaveUser();
 
+        sv.insert(inae);
+        sv.insert(nina);
 
-
-
-
-
+        inae.toString();
+        System.out.println(nina.getName());
+        System.out.println(nina.getEmail());
+        System.out.println(nina.getID());
 
     }
 }
